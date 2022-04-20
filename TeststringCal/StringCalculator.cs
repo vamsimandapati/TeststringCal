8dd23f8e5946ac626e84c118cffa3f183f410610
MV65550 <MV65550@APPW46ZLPC1112>
2022-04-20 13:15:37 +0530

    Add project files.

---
+++ TeststringCal/StringCalculator.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TeststringCal
+{
+    public class StringCalculator
+    {
+        internal object Add(string numbers)
+        {
+            if (String.IsNullOrEmpty(numbers)) return 0;
+
+            var delimiters = new List<char> { ',', '\n' };
+
+            string numberString = numbers;
+            if (numberString.StartsWith("//"))
+            {
+                var splitInput = numberString.Split('\n');
+                var newDelimiter = splitInput.First().Trim('/');
+                numberString = String.Join('\n', splitInput.Skip(1));
+
+                delimiters.Add(Convert.ToChar(newDelimiter));
+            }
+
+            var numberList = numberString.Split(delimiters.ToArray())
+                .Select(s => int.Parse(s));
+
+            var negatives = numberList
+                .Where(n => n < 0);
+
+            if (negatives.Any())
+            {
+                string negativeString = String.Join(',', negatives
+                    .Select(n => n.ToString()));
+                throw new Exception($"Negatives not allowed: {negativeString}");
+            }
+
+            var result = numberList
+                .Where(n => n <= 1000)
+                .Sum();
+
+            return result;
+        }
+    }
+}
---
+++ TeststringCal/StringCalculator_Add.cs
@@ -0,0 +1,85 @@
+using System;
+
+using Xunit;
+
+namespace TeststringCal
+{
+     public class StringCalculator_Add
+    {
+       private StringCalculator _calculator = new StringCalculator();
+
+        [Fact]
+        public void Returns0GivenEmptyString()
+        {
+          var result = _calculator.Add("");
+
+             Assert.Equal(0, result);
+        }
+
+        [Theory]
+        [InlineData("1", 1)]
+        [InlineData("2", 2)]
+        public void ReturnsNumberGivenStringWithOneNumber(string numbers, int expectedResult)
+        {
+            var result = _calculator.Add(numbers);
+
+            Assert.Equal(expectedResult, result);
+        }
+
+        [Theory]
+        [InlineData("1,2", 3)]
+        [InlineData("2,3", 5)]
+        public void ReturnsSumGivenStringWithTwoCommaSeparatedNumbers(string numbers, int expectedResult)
+        {
+            var result = _calculator.Add(numbers);
+
+            Assert.Equal(expectedResult, result);
+        }
+
+        [Theory]
+        [InlineData("1,2,3", 6)]
+        [InlineData("2,3,4", 9)]
+        public void ReturnsSumGivenStringWithThreeCommaSeparatedNumbers(string numbers, int expectedResult)
+        {
+            var result = _calculator.Add(numbers);
+
+            Assert.Equal(expectedResult, result);
+        }
+
+        [Theory]
+        [InlineData("1\n2,3", 6)]
+        [InlineData("1\n2\n3", 6)]
+        [InlineData("1,2\n3", 6)]
+        public void ReturnsSumGivenStringWithThreeCommaOrNewlineSeparatedNumbers(string numbers, int expectedResult)
+        {
+            var result = _calculator.Add(numbers);
+
+            Assert.Equal(expectedResult, result);
+        }
+
+        [Theory]
+        [InlineData("//;\n1;2;3", 6)]
+        public void ReturnsSumGivenStringWithCustomDelimiter(string numbers, int expectedResult)
+        {
+            var result = _calculator.Add(numbers);
+
+            Assert.Equal(expectedResult, result);
+        }
+
+        [Theory]
+        [InlineData("-1,2", "Negatives not allowed: -1")]
+        [InlineData("-1,-2", "Negatives not allowed: -1,-2")]
+        public void ThrowsGivenNegativeInputs(string numbers, string expectedMessage)
+        {
+
+            Action action = () => _calculator.Add(numbers);
+
+            var ex = Assert.Throws<Exception>(action);
+
+            Assert.Equal(expectedMessage, ex.Message);
+        }
+
+        
+
+    }
+}
